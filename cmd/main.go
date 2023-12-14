ca281a52611993af55445ea3203db1f4a2f7cf49
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-12-14 11:52:30 +0530

    Update AWS SDK and add Lambda integration

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,26 @@
 package main
 
-import "os"
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
+	//"github.com/aws/aws-sdk-go/service/lambda"
+)
 
+func main() {
 
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
 
-func main(){
+	if err != nil {
+		return
+	}
 
-	region:=os.Getenv("AWS_REGION")
-	
+	dynaClient := dynamodb.New(awsSession)
+
+	lambda.Start(handler)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/to4to/go-serverless-api
 go 1.21.1
 
 require (
-	github.com/aws/aws-sdk-go v1.48.16 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/aws/aws-lambda-go v1.42.0
+	github.com/aws/aws-sdk-go v1.48.16
 )
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
