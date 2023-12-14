b753d96b6dd605d0d161b10a6f407776fee27f7c
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-12-14 12:02:32 +0530

    Add AWS Lambda handler for GET request

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/to4to/go-serverless-api/pkg/handlers"
 	"os"
 	//"github.com/aws/aws-sdk-go/service/lambda"
 )
 
 func main() {
 
+	// region := os.Getenv("AWS_REGION")
+	// awsSession, err := session.NewSession(&aws.Config{
+	// 	Region: aws.String(region),
+	// })
+
+	// if err != nil {
+	// 	return
+	// }
+
+	// dynaClient := dynamodb.New(awsSession)
+
+	lambda.Start(handler)
+}
+
+const tableName = "LambdaInGoUser"
+
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	region := os.Getenv("AWS_REGION")
 	awsSession, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
 	})
 
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	dynaClient := dynamodb.New(awsSession)
+	switch req.HTTPMethod {
 
-	lambda.Start(handler)
+	case "GET":
+		return handlers.GetUser(req, tableName, dynaClient)
+	}
 }
