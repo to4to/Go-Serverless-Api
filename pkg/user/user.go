62901adc9e5baa197d9a2adce8fd6a18a848efae
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-02-07 22:36:41 +0530

    Add FetchUser function to retrieve user from DynamoDB

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,20 +1,16 @@
 package user
 
-
-
-
-type User struct{
-	Email string `json:"email"`
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName`
-}
+import "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 
 var ()
 
+type User struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName`
+}
 
-
-
-func FetchUser() {
+func FetchUser(email,tableName string,dynalient dynamodbiface.DynamoDBAPI)(*User,error) {
 
 }
 
