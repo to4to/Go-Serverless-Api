46223cd8b1c36ca5c14922b8deb0cf7f25120bf0
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-02-12 12:43:11 +0530

    Update error messages in user.go

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -11,9 +11,15 @@ import (
 )
 
 var (
-	ErrorFailedToFetchRecord = "Failed To Fetch Record"
-
-	ErrorFailedToUnMArshalRecord = "Failed To UnMrashal"
+	ErrorFailedToUnmarshalRecord = "failed to unmarshal record"
+	ErrorFailedToFetchRecord = "failed to fetch record"
+	ErrorInvalidUserData = "invalid user data"
+	ErrorInvalidEmail = "invalid email"
+	ErrorCouldNotMarshalItem = "could not marshal item"
+	ErrorCouldNotDeleteItem = "could not delete item"
+	ErrorCouldNotDynamoPutItem = "could not dynamo put item"
+	ErrorUserAlreadyExists = "user.User already exists"
+	ErrorUserDoesNotExist = "user.User does not exist"
 )
 
 type User struct {
