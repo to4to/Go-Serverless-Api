6be221e1e4db646b2d87731ee833973c8218b8b4
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-02-20 12:11:15 +0530

    Update USer Complete

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -132,6 +132,22 @@ func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient 
 		return nil,errors.New(ErrorCouldNotMarshalItem)
 	}
 
+
+
+	input:=&dynamodb.PutItemInput{
+		Item: av,
+		TableName: aws.String(tableName),
+		
+	}
+
+	dynaClient.PutItem(input)
+
+
+	if err!=nil{
+		return nil,errors.New(ErrorCouldNotDynamoPutItem)
+	}
+
+	return &u,nil
 }
 
 func DeleteUser() error {
