640a5a6be5812cbcde23b1151c06ee6f065b9a0e
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2024-02-21 23:51:03 +0530

    Starting Delete User

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -150,7 +150,7 @@ func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient 
 	return &u,nil
 }
 
-func DeleteUser() error {
+func DeleteUser(req events.APIGatewayProxyRequest) error {
 
 	return nil
 }
